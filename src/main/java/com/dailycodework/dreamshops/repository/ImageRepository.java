2c89fe80ba7b3e726f4fdd1a221492ed6cb9b113
malish-stha <malish.shrestha@deerwalk.edu.np>
2025-02-04 15:42:55 +0545

    Implement ImageService, ImageRepository, IImageService Interface

---
+++ src/main/java/com/dailycodework/dreamshops/repository/ImageRepository.java
@@ -0,0 +1,8 @@
+package com.dailycodework.dreamshops.repository;
+
+import com.dailycodework.dreamshops.model.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ImageRepository extends JpaRepository<Image, Long> {
+
+}
