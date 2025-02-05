f77ad63d4f46375e68be0346c3e5cb9b86b199c6
malish-stha <malish.shrestha@deerwalk.edu.np>
2025-02-05 16:03:52 +0545

    Add UserService UserRepository ...

---
+++ src/main/java/com/dailycodework/dreamshops/service/user/UserService.java
@@ -0,0 +1,54 @@
+package com.dailycodework.dreamshops.service.user;
+
+import com.dailycodework.dreamshops.exceptions.AlreadyExistsException;
+import com.dailycodework.dreamshops.exceptions.ResourceNotFoundException;
+import com.dailycodework.dreamshops.model.User;
+import com.dailycodework.dreamshops.repository.UserRepository;
+import com.dailycodework.dreamshops.request.CreateUserRequest;
+import com.dailycodework.dreamshops.request.UserUpdateRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserService implements IUserService{
+    private final UserRepository userRepository;
+
+    @Override
+    public User getUserById(Long userId) {
+        return userRepository.findById(userId).orElseThrow(()-> new ResourceNotFoundException("User not found!!"));
+    }
+
+    @Override
+    public User createUser(CreateUserRequest request) {
+        return Optional.of(request)
+                .filter(
+                        user-> !userRepository.existsByEmail(request.getEmail())).map(req -> {
+                            User user = new User();
+                            user.setEmail(req.getEmail());
+                            user.setFirstName(req.getFirstName());
+                            user.setLastName(req.getLastName());
+                            return userRepository.save(user);
+                }).orElseThrow(()-> new AlreadyExistsException("User "+ request.getEmail() + " already exists!!"));
+
+    }
+
+    @Override
+    public User updateUser(UserUpdateRequest request, Long userId) {
+        return userRepository.findById(userId).map(existingUser ->{
+            existingUser.setFirstName(request.getFirstName());
+            existingUser.setLastName(request.getLastName());
+            return userRepository.save(existingUser);
+        }).orElseThrow(()-> new ResourceNotFoundException("User not found!!"));
+
+    }
+
+    @Override
+    public void deleteUser(Long userId) {
+        userRepository.findById(userId).ifPresentOrElse(userRepository::delete, ()-> {
+            throw new ResourceNotFoundException("User not found!!");
+        });
+    }
+}
